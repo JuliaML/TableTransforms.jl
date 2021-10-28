8b4915ddf30a428959b04b620d3e45560c5d4514
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-10-28 10:41:31 -0300

    Refactor Scale

--- src/transforms/scale.jl
+++ src/transforms/scale.jl
@@ -18,30 +18,13 @@ Scale(; low=0.25, high=0.75) = Scale(low, high)
 isrevertible(::Type{Scale}) = true
 
 function apply(transform::Scale, table)
-  # sanity checks
   assert_continuous(table)
-
-  # variable names
-  names = schema(table).names
-
-  # scaled values and factors
-  vals = map(names) do name
-    x = Tables.getcolumn(table, name)
+  colwise(table) do x
     levels = (transform.low, transform.high)
     xl, xh = quantile(x, levels)
     z = (x .- xl) ./ (xh .- xl)
     z, (xl=xl, xh=xh)
   end
-
-  # table with scaled values
-  𝒯 = (; zip(names, first.(vals))...)
-  newtable = 𝒯 |> Tables.materializer(table)
-
-  # vector with scaling factors
-  factors = last.(vals)
-
-  # return scaled table and factors
-  newtable, factors
 end
 
 function revert(::Scale, newtable, cache)
