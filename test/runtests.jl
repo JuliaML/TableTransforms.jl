f7a6a7a84a29e8d95ee9beeacf8b04c5601076ce
José A. S. Silva <jass2@ic.ufal.br>
2021-10-28 12:54:08 -0300

    Adds Tables, LinearAlgebra and Statistics as dependecies, needed in the eigenanalysis testset. Writes the testset for eigenanalysis.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using TableTransforms
 using Distributions
 using Test
+using Tables
+using Statistics
+using LinearAlgebra
 
 # list of tests
 testfiles = [
--- test/transforms.jl
+++ test/transforms.jl
@@ -16,7 +16,67 @@
   end
 
   @testset "EigenAnalysis" begin
-    # TODO
+    # test PCA direct and inverse transformation
+    t = (a = rand(1000), b = rand(1000))
+    X = Tables.matrix(t)
+    μ = mean(X, dims=1)
+    Σ = cov(X)
+    _, V = eigen(Σ)
+    Vᵀ = transpose(V)
+
+    tₙ, c = apply(EigenAnalysis(:V), t)
+    Xₙ = Tables.matrix(tₙ)
+
+    tₒ = revert(EigenAnalysis(:V), tₙ, c)
+    Xₒ = Tables.matrix(tₒ)
+    @test Xₙ ≈ (X .- μ) * V
+    @test first(c) ≈ Vᵀ
+    @test last(c) ≈ μ
+    @test Xₒ ≈ (Xₙ * Vᵀ .+ μ)
+    @test X ≈ Xₒ
+
+    # test DRS direct and inverse transformation
+    t = (a = rand(1000), b = rand(1000))
+    X = Tables.matrix(t)
+    μ = mean(X, dims=1)
+    Σ = cov(X)
+    λ, V = eigen(Σ)
+    Λ = Diagonal(sqrt.(λ))
+    S = V * inv(Λ)
+    Sⁱ = inv(S)
+
+    tₙ, c = apply(EigenAnalysis(:VD), t)
+    Xₙ = Tables.matrix(tₙ)
+
+    tₒ = revert(EigenAnalysis(:VD), tₙ, c)
+    Xₒ = Tables.matrix(tₒ)
+    @test Xₙ ≈ (X .- μ) * S   # test the result matrix manually
+    @test first(c) ≈ Sⁱ       # test if the inverse matrix is computed correctly
+    @test last(c) ≈ μ         # test if the means is computed correctly
+    @test Xₒ ≈ (Xₙ * Sⁱ .+ μ) # test the revert is working fine
+    @test X ≈ Xₒ              # test the back-transform is giving X back
+
+    # test SDS direct and inverse transformation
+    t = (a = rand(1000), b = rand(1000))
+    X = Tables.matrix(t)
+    μ = mean(X, dims=1)
+    Σ = cov(X)
+    λ, V = eigen(Σ)
+    Λ = Diagonal(sqrt.(λ))
+    Vᵀ = transpose(V)
+    S = V * inv(Λ) * Vᵀ
+    Sⁱ = inv(S)
+
+    tₙ, c = apply(EigenAnalysis(:VDV), t)
+    Xₙ = Tables.matrix(tₙ)
+
+    tₒ = revert(EigenAnalysis(:VDV), tₙ, c)
+    Xₒ = Tables.matrix(tₒ)
+    @test Xₙ ≈ (X .- μ) * S   # test the result matrix manually
+    @test first(c) ≈ Sⁱ       # test if the inverse matrix is computed correctly
+    @test last(c) ≈ μ         # test if the means is computed correctly
+    @test Xₒ ≈ (Xₙ * Sⁱ .+ μ) # test the revert is working fine
+    @test X ≈ Xₒ              # test the back-transform is giving X back
   end
 
   @testset "Sequential" begin
