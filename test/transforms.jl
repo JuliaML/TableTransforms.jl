810f7547813917b75f900861f0c6df188d8fde94
José A. S. Silva <jass2@ic.ufal.br>
2021-10-28 21:01:31 -0300

    move plots to inside if of visual tests. update [:, :symbol] to .symbol

--- test/transforms.jl
+++ test/transforms.jl
@@ -21,11 +21,11 @@
     tₒ = revert(Center(), n, c)
     @test t ≈ tₒ
 
-    # visual tests
-    pₒ = scatter(t[:, :x], t[:, :y], label="Original")
-    p = scatter(n[:, :x], n[:, :y], label="Center")
-    
+    # visual tests    
     if visualtests
+      pₒ = scatter(t.x, t.y, label="Original")
+      p = scatter(n.x, n.y, label="Center")
+
       @test_reference joinpath(datadir,  "center.png") plot(pₒ, p, layout=(1,2))
     end
   end
@@ -49,11 +49,11 @@
     tₒ = revert(ZScore(), n, c)
     @test t ≈ tₒ
 
-    # visual tests
-    pₒ = scatter(t[:, :x], t[:, :y], label="Original")
-    p = scatter(n[:, :x], n[:, :y], label="ZScore")
-    
+    # visual tests   
     if visualtests
+      pₒ = scatter(t.x, t.y, label="Original")
+      p = scatter(n.x, n.y, label="ZScore")
+
       @test_reference joinpath(datadir,"zscore.png") plot(pₒ, p, layout=(1,2))
     end
   end
@@ -98,26 +98,27 @@
     tₒ = revert(EigenAnalysis(:VDV), n, c)
     @test t ≈ tₒ
 
-    # visual tests
-    Random.seed!(42) # to reproduce the results
-    x = rand(Normal(0,10), 4000)
-    y = x + rand(Normal(0,2), 4000)
-    t = DataFrame(:x => x, :y => y)
-    t₁, c₁ = apply(EigenAnalysis(:V), t)
-    t₂, c₂ = apply(EigenAnalysis(:VD), t)
-    t₃, c₃ = apply(EigenAnalysis(:VDV), t)
-    t₄, c₄ = apply(PCA(), t)
-    t₅, c₅ = apply(DRS(), t)
-    t₆, c₆ = apply(SDS(), t)
-    pₒ = scatter(t[:, :x], t[:, :y], label="Original")
-    p₁ = scatter(t₁[:, :x], t₁[:, :y], label="V")
-    p₂ = scatter(t₂[:, :x], t₂[:, :y], label="VD")
-    p₃ = scatter(t₃[:, :x], t₃[:, :y], label="VDV")
-    p₄ = scatter(t₄[:, :x], t₄[:, :y], label="PCA")
-    p₅ = scatter(t₅[:, :x], t₅[:, :y], label="DRS")
-    p₆ = scatter(t₆[:, :x], t₆[:, :y], label="SDS")
-    
+    # visual tests    
     if visualtests
+      Random.seed!(42) # to reproduce the results
+      x = rand(Normal(0,10), 4000)
+      y = x + rand(Normal(0,2), 4000)
+      t = DataFrame(:x => x, :y => y)
+      t₁, c₁ = apply(EigenAnalysis(:V), t)
+      t₂, c₂ = apply(EigenAnalysis(:VD), t)
+      t₃, c₃ = apply(EigenAnalysis(:VDV), t)
+      t₄, c₄ = apply(PCA(), t)
+      t₅, c₅ = apply(DRS(), t)
+      t₆, c₆ = apply(SDS(), t)
+
+      pₒ = scatter(t.x, t.y, label="Original")
+      p₁ = scatter(t₁.x, t₁.y, label="V")
+      p₂ = scatter(t₂.x, t₂.y, label="VD")
+      p₃ = scatter(t₃.x, t₃.y, label="VDV")
+      p₄ = scatter(t₄.x, t₄.y, label="PCA")
+      p₅ = scatter(t₅.x, t₅.y, label="DRS")
+      p₆ = scatter(t₆.x, t₆.y, label="SDS")
+
       @test_reference joinpath(datadir,"eigenanalysis-1.png") plot(pₒ, p₁, p₂, p₃, layout=(2,2))
       @test_reference joinpath(datadir,"eigenanalysis-2.png") plot(p₁, p₂, p₃, p₄, p₅, p₆, layout=(2,3))
     end
