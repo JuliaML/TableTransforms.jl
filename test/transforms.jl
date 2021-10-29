9bacb11136c1543291695d2fbc4dae851d6c3a2f
José A. S. Silva <32930332+mrr00b00t@users.noreply.github.com>
2021-10-29 13:09:46 -0300

    replace DataFrames with TypedTables. Write testes for Select and Reject. (#6)
    * replace DataFrames with TypedTables. Write testes for Select and Reject.
    
    * rewrite test
    
    * more tests to Select and Reject

--- test/transforms.jl
+++ test/transforms.jl
@@ -2,24 +2,94 @@
   @testset "Identity" begin
     x = rand(4000)
     y = rand(4000)
-    t = DataFrame(:x => x, :y => y)
+    t = Table(; x, y)
     n, c = apply(Identity(), t)
     @test t == n
     tₒ = revert(Identity(), n, c)
     @test t == tₒ
   end
 
+  @testset "Select" begin
+    a = rand(4000)
+    b = rand(4000)
+    c = rand(4000)
+    d = rand(4000)
+    e = rand(4000)
+    f = rand(4000)
+    t = Table(; a, b, c, d, e, f)
+
+    n₁, c₁ = apply(Select(:f, :d), t)
+    n₂, c₂ = apply(Select(:f, :d, :b), t)
+    n₃, c₃ = apply(Select(:d, :c, :b), t)
+    n₄, c₄ = apply(Select(:e, :c, :b, :a), t)
+
+    u₁ = Tables.columnnames(n₁)
+    u₂ = Tables.columnnames(n₂)
+    u₃ = Tables.columnnames(n₃)
+    u₄ = Tables.columnnames(n₄)
+
+    @test u₁ == (:f, :d)
+    @test u₂ == (:f, :d, :b)
+    @test u₃ == (:d, :c, :b)
+    @test u₄ == (:e, :c, :b, :a)
+
+    tₒ₁ = revert(Select(:f, :d), n₁, c₁)
+    tₒ₂ = revert(Select(:f, :d, :b), n₂, c₂)
+    tₒ₃ = revert(Select(:d, :c, :b), n₃, c₃)
+    tₒ₄ = revert(Select(:e, :c, :b, :a), n₄, c₄)
+
+    @test t == tₒ₁
+    @test t == tₒ₂
+    @test t == tₒ₃
+    @test t == tₒ₄
+  end
+
+  @testset "Reject" begin
+    a = rand(4000)
+    b = rand(4000)
+    c = rand(4000)
+    d = rand(4000)
+    e = rand(4000)
+    f = rand(4000)
+    t = Table(; a, b, c, d, e, f)
+
+    n₁, c₁ = apply(Reject(:f, :d), t)
+    n₂, c₂ = apply(Reject(:f, :d, :b), t)
+    n₃, c₃ = apply(Reject(:d, :c, :b), t)
+    n₄, c₄ = apply(Reject(:e, :c, :b, :a), t)
+
+    u₁ = Tables.columnnames(n₁)
+    u₂ = Tables.columnnames(n₂)
+    u₃ = Tables.columnnames(n₃)
+    u₄ = Tables.columnnames(n₄)
+
+    @test u₁ == (:a, :b, :c, :e)
+    @test u₂ == (:a, :c, :e)
+    @test u₃ == (:a, :e, :f)
+    @test u₄ == (:d, :f)
+
+    tₒ₁ = revert(Reject(:f, :d), n₁, c₁)
+    tₒ₂ = revert(Reject(:f, :d, :b), n₂, c₂)
+    tₒ₃ = revert(Reject(:d, :c, :b), n₃, c₃)
+    tₒ₄ = revert(Reject(:e, :c, :b, :a), n₄, c₄)
+
+    @test t == tₒ₁
+    @test t == tₒ₂
+    @test t == tₒ₃
+    @test t == tₒ₄
+  end
+
   @testset "Center" begin
     Random.seed!(42) # to reproduce the results
     x = rand(Normal(2,1), 4000)
     y = rand(Normal(5,1), 4000)
-    t = DataFrame(:x => x, :y => y)
+    t = Table(; x, y)
     n, c = apply(Center(), t)
     μ = mean(Tables.matrix(n), dims=1)
     @test isapprox(μ[1], 0; atol=1e-6)
     @test isapprox(μ[2], 0; atol=1e-6)
     tₒ = revert(Center(), n, c)
-    @test t ≈ tₒ
+    @test Tables.matrix(t) ≈ Tables.matrix(tₒ)
 
     # visual tests    
     if visualtests
@@ -35,14 +105,14 @@
     Random.seed!(42) # to reproduce the results
     x = rand(Normal(4,3), 4000)
     y = rand(Normal(7,5), 4000)
-    t = DataFrame(:x => x, :y => y)
+    t = Table(; x, y)
     n, c = apply(Scale(low=0, high=1), t)
     @test all(x -> x <= 1, n.x)
     @test all(x -> x >= 0, n.x)
     @test all(y -> y <= 1, n.y)
     @test all(y -> y >= 0, n.y)
     tₒ = revert(Scale(low=0, high=1), n, c)
-    @test t ≈ tₒ
+    @test Tables.matrix(t) ≈ Tables.matrix(tₒ)
 
     # visual tests   
     if visualtests
@@ -58,7 +128,7 @@
     Random.seed!(42) # to reproduce the results
     x = rand(Normal(7,10), 4000)
     y = rand(Normal(15,2), 4000)
-    t = DataFrame(:x => x, :y => y)
+    t = Table(; x, y)
     n, c = apply(ZScore(), t)
     μ = mean(Tables.matrix(n), dims=1)
     σ = std(Tables.matrix(n), dims=1)
@@ -67,7 +137,7 @@
     @test isapprox(μ[2], 0; atol=1e-6)
     @test isapprox(σ[2], 1; atol=1e-6)
     tₒ = revert(ZScore(), n, c)
-    @test t ≈ tₒ
+    @test Tables.matrix(t) ≈ Tables.matrix(tₒ)
 
     # visual tests   
     if visualtests
@@ -83,7 +153,7 @@
     # PCA test
     x = rand(Normal(0,10), 1500)
     y = x + rand(Normal(0,2), 1500)
-    t = DataFrame(:x => x, :y => y)
+    t = Table(; x, y)
     n, c = apply(EigenAnalysis(:V), t)
     Σ = cov(Tables.matrix(n))
     @test Σ[1,1] > 1
@@ -91,12 +161,12 @@
     @test isapprox(Σ[2,1], 0; atol=1e-6)
     @test Σ[2,2] > 1
     tₒ = revert(EigenAnalysis(:V), n, c)
-    @test t ≈ tₒ
+    @test Tables.matrix(t) ≈ Tables.matrix(tₒ)
 
     # DRS test
     x = rand(Normal(0,10), 1500)
     y = x + rand(Normal(0,2), 1500)
-    t = DataFrame(:x => x, :y => y)
+    t = Table(; x, y)
     n, c = apply(EigenAnalysis(:VD), t)
     Σ = cov(Tables.matrix(n))
     @test isapprox(Σ[1,2], 0; atol=1e-6)
@@ -104,12 +174,12 @@
     @test isapprox(Σ[1,1], 1; atol=1e-6)
     @test isapprox(Σ[2,2], 1; atol=1e-6)
     tₒ = revert(EigenAnalysis(:VD), n, c)
-    @test t ≈ tₒ
+    @test Tables.matrix(t) ≈ Tables.matrix(tₒ)
 
     # SDS test
     x = rand(Normal(0,10), 1500)
     y = x + rand(Normal(0,2), 1500)
-    t = DataFrame(:x => x, :y => y)
+    t = Table(; x, y)
     n, c = apply(EigenAnalysis(:VDV), t)
     Σ = cov(Tables.matrix(n))
     @test isapprox(Σ[1,2], 0; atol=1e-6)
@@ -117,12 +187,12 @@
     @test isapprox(Σ[1,1], 1; atol=1e-6)
     @test isapprox(Σ[2,2], 1; atol=1e-6)
     tₒ = revert(EigenAnalysis(:VDV), n, c)
-    @test t ≈ tₒ
+    @test Tables.matrix(t) ≈ Tables.matrix(tₒ)
 
     Random.seed!(42) # to reproduce the results
     x = rand(Normal(0,10), 4000)
     y = x + rand(Normal(0,2), 4000)
-    t₁ = DataFrame(:x => x, :y => y)
+    t₁ = Table(; x, y)
     t₂, c₂ = apply(EigenAnalysis(:V), t₁)
     t₃, c₃ = apply(EigenAnalysis(:VD), t₁)
     t₄, c₄ = apply(EigenAnalysis(:VDV), t₁)
