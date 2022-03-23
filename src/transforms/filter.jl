eede0ada20ddcc8c0bfec304fbd0dd5a26c687ea
ceferisbarov <-l>
2022-03-23 12:30:33 +0400

    Revised the Filter transform

--- src/transforms/filter.jl
+++ src/transforms/filter.jl
@@ -11,13 +11,22 @@ struct Filter{F} <: Transform
   pred::F
 end
 
-Filter() = Filter(_default_filter)
+# Default filter to be used when no input function is given
+# Returns true if any element in the row evaluates to true
+Filter() = Filter(row -> any(row))
 
 isrevertible(::Type{Filter}) = true
 
 function apply(transform::Filter, input_table)
+  # Converting to Tables.rowtable to allow length function and indexing 
   table = Tables.rowtable(input_table)
+
+  # Get indices of the desired rows
   indices = [i for i in range(1,length=length(table)) if transform.pred(table[i])]
+
+  # Return:
+  # - the desired rows as the new table,
+  # - indices of these rows and the remaining rows as cache
   table[indices], (indices, table[setdiff(1:length(table), indices)])
 end
 
@@ -30,13 +39,4 @@ function revert(::Type{Filter}, newtable, cache)
   end
 
   orgtable
-end
-
-# Exclude the given indices
-_minus(indx, x) = setdiff(1:length(x), indices)
-
-# Default filter to be used when no input function is given
-# Returns true if any element in the row evaluates to true
-function _default_filter(row)
-  return any(row)
 end
