df8d69db70ca1fc4f9f6ac104f8db9fd876869c1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-03-30 13:54:47 -0300

    Update src/transforms/utils.jl

--- src/transforms/utils.jl
+++ src/transforms/utils.jl
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------
 
 const ColSelector = Union{Symbol, Integer, AbstractString}
-const ColSpec = Union{Vector{T}, NTuple{N, T}, Regex, Colon} where {N, T<:ColSelector}
+const ColSpec = Union{Vector{T}, NTuple{N,T}, Regex, Colon} where {N,T<:ColSelector}
 
 _select(colspec::Vector{Symbol}, allcols) = colspec
 _select(colspec::Vector{<:Integer}, allcols) = allcols[colspec]
