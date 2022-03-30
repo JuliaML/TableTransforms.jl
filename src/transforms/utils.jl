6a09acd9a3333b6d8b3928efda4ffed4cafeadec
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-03-30 13:54:25 -0300

    Update src/transforms/utils.jl

--- src/transforms/utils.jl
+++ src/transforms/utils.jl
@@ -8,7 +8,7 @@ const ColSpec = Union{Vector{T}, NTuple{N, T}, Regex, Colon} where {N, T<:ColSel
 _select(colspec::Vector{Symbol}, allcols) = colspec
 _select(colspec::Vector{<:Integer}, allcols) = allcols[colspec]
 _select(colspec::Vector{<:AbstractString}, allcols) = Symbol.(colspec)
-_select(colspec::NTuple{N, <:ColSelector}, allcols) where {N} =
+_select(colspec::NTuple{N,<:ColSelector}, allcols) where {N} =
   _select(collect(colspec), allcols)
 _select(colspec::Regex, allcols) = 
   filter(col -> occursin(colspec, String(col)), allcols)
