4172af153675c75da5c17559cc9f9e879503e9c7
ceferisbarov <-l>
2022-04-04 17:06:33 +0400

    Added assert_continuous_or_missing function & revised Center transform to support missing values & fixes #27

--- src/assertions.jl
+++ src/assertions.jl
@@ -7,3 +7,8 @@ function assert_continuous(table)
   types = schema(table).scitypes
   @assert all(T <: Continuous for T in types) "columns must hold continuous variables"
 end
+
+function assert_continuous_or_missing(table)
+  types = schema(table).scitypes
+  @assert all(T <: Union{Missing, Continuous} for T in types) "columns must hold continuous variables"
+end
--- src/transforms/center.jl
+++ src/transforms/center.jl
@@ -3,17 +3,28 @@
 # ------------------------------------------------------------------
 
 """
-    Center()
+    Center(skipmissing=true)
 
-The transform that removes the mean of the variables.
+The transform that removes the mean of the variables. Skips the missing values by default.
 """
-struct Center <: Colwise end
 
-assertions(::Type{Center}) = [assert_continuous]
+struct Center <: Colwise
+    skipmissing::Bool
+end
+
+Center(;skipmissing=true) = Center(skipmissing)
+
+assertions(::Type{Center}) = [assert_continuous_or_missing]
 
 isrevertible(::Type{Center}) = true
 
-colcache(::Center, x) = mean(x)
+function colcache(transform::Center, x)
+    if transform.skipmissing
+        mean(skipmissing(x))
+    else
+        mean(x)
+    end
+end
 
 colapply(::Center, x, μ)  = @. x - μ
 
