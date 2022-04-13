902944cf754ca46d11727ff9165b2c3dec9bfe78
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-04-13 16:55:32 -0300

    Add whiteline

--- test/transforms.jl
+++ test/transforms.jl
@@ -755,6 +755,7 @@
     @test ntypes[6] == Int
     @test ttypes == Tables.schema(tₒ).types    
   end
+  
   @testset "Coerce" begin
     x1 = [1.0, 2.0, 3.0, 4.0, 5.0]
     x2 = [1.0, 2.0, 3.0, 4.0, 5.0]
