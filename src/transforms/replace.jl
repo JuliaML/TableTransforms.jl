25d27d05e18c7f23b76e1b69dcaec2bcff2433c1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-04-20 08:51:50 -0300

    Update src/transforms/replace.jl

--- src/transforms/replace.jl
+++ src/transforms/replace.jl
@@ -10,8 +10,8 @@ Replaces `oldᵢ` value with `newᵢ` value in the table.
 # Examples
 
 ```julia
-T = Replace(1 => -1, 5 => -5)
-T = Replace(1 => 1.5, 5 => 5.5, 4 => true)
+Replace(1 => -1, 5 => -5)
+Replace(1 => 1.5, 5 => 5.5, 4 => true)
 ```
 """
 struct Replace{K,V} <: Colwise
