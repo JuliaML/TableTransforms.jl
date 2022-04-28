5d6ba026453f64d31a04f9592688bb2705948939
Elias Carvalho <eliascarvdev@gmail.com>
2022-04-28 11:00:01 -0300

    WIP: New Functional

--- src/transforms/functional.jl
+++ src/transforms/functional.jl
@@ -3,43 +3,75 @@
 # ------------------------------------------------------------------
 
 """
-    Functional(function)
+    Functional(func)
 
-The transform that applies a `function` elementwise.
+The transform that applies a `func` elementwise.
+
+    Functional(col₁ => func₁, col₂ => func₂, ..., colₙ => funcₙ)
+
+Applies in each `colᵢ` the function `funcᵢ` in the table.
 
 # Examples
 
 ```julia
 Functional(cos)
 Functional(sin)
+Functional(:a => cos, :b => sin)
+Functional("a" => cos, "b" => sin)
 ```
 """
-struct Functional{F} <: Colwise
-  func::F
+struct Functional{K,F} <: Stateless
+  pairs::Dict{K,F}
 end
 
-isrevertible(transform::Functional) =
-  !isnothing(inverse(transform.func))
+Functional(func) = Functional(Dict((:) => func))
+
+Functional(pairs::Pair{Symbol,F}...) where {F} =
+  Functional(Dict(pairs))
+
+Functional(pairs::Pair{K,F}...) where {K<:AbstractString,F} =
+  Functional(Dict(Symbol(k) => v for (k, v) in pairs))
+
+Functional() = throw(ArgumentError("Cannot create a Functional object without arguments."))
 
 # known invertible functions
-inverse(::typeof(log))  = exp
-inverse(::typeof(exp))  = log
-inverse(::typeof(cos))  = acos
-inverse(::typeof(acos)) = cos
-inverse(::typeof(sin))  = asin
-inverse(::typeof(asin)) = sin
+inverse(::typeof(log))   = exp
+inverse(::typeof(exp))   = log
+inverse(::typeof(cos))   = acos
+inverse(::typeof(acos))  = cos
+inverse(::typeof(sin))   = asin
+inverse(::typeof(asin))  = sin
+inverse(::typeof(cosd))  = acosd
+inverse(::typeof(acosd)) = cosd
+inverse(::typeof(sind))  = asind
+inverse(::typeof(asind)) = sind
 
 # fallback to nothing
 inverse(::Any) = nothing
 
-colcache(::Functional, x) = nothing
+isrevertible(transform::Functional{Colon}) =
+  !isnothing(inverse(transform.pairs[:]))
 
-function colapply(transform::Functional, x, c)
-  f = transform.func
-  f.(x)
+isrevertible(transform::Functional) =
+  any(isnothing, inverse.(values(transform.pairs)))
+
+function applyfunc(transform::Functional, cols, nm)
+  x = Tables.getcolumn(cols, nm)
+  func = get(transform.pairs, nm, identity)
+  func.(x)
 end
 
-function colrevert(transform::Functional, y, c)
-  g = inverse(transform.func)
-  g.(y)
+function applyfunc(transform::Functional{Colon}, cols, nm)
+  x = Tables.getcolumn(cols, nm)
+  func = transform.pairs[:]
+  func.(x)
+end
+
+function apply(transform::Functional, table) 
+  cols = Tables.getcolumns(table)
+  names = Tables.columnnames(table)
+  ncols = tcollect(applyfunc(transform, cols, nm) for nm in names)
+  𝒯 = (; zip(names, ncols)...)
+  newtable = 𝒯 |> Tables.materializer(table)
+  return newtable, nothing
 end
