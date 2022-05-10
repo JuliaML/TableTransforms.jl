0268afda467810981153a617c104c815d6cba4fd
ceferisbarov <-l>
2022-05-10 14:49:52 +0400

    Revised Rename transform to accept Dictionary of strings

--- src/transforms/rename.jl
+++ src/transforms/rename.jl
@@ -18,8 +18,14 @@ struct Rename <: Stateless
   names::Dict{Symbol,Symbol}
 end
 
+Rename(names::Dict) = _symboldict(names) |> Rename
 Rename(names::Pair) = _pairsyms(names) |> Dict |> Rename
 Rename(names...) = _pairsyms.(names) |> Dict |> Rename
+
+function _symboldict(names)
+  f((k,v)) = Symbol(k) => Symbol(v)
+  Dict(Iterators.map(f, pairs(names)))
+end
 
 _pairsyms(x::Pair) = Symbol(first(x)) => Symbol(last(x))
 
