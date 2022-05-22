68106b0feddd287f46b39a759dceefaefad9e6fd
frankwang <1454884738@qq.com>
2022-05-22 20:19:24 +0800

    name changes

--- src/transforms/levels.jl
+++ src/transforms/levels.jl
@@ -1,6 +1,5 @@
-#-----todo the things that I should work on is the constructor
 """
-		Levels(:a => ["yes,"no"])
+    Levels(:a => ["yes,"no"])
 
 Return a copy of the table with specified levels and orders for categorical columns
 allowing only changing the order of the column
@@ -9,7 +8,7 @@ allowing only changing the order of the column
 
 ```julia
 Levels(:a => ["yes,"no"], :c => [1,2,4], :d = ["a","b","c"])
-Levels("a" => ["yes","no"], "c" => [1,2,4],ordered= ["a","c"])
+Levels("a" => ["yes","no"], "c" => [1,2,4], ordered= ["a","c"])
 Levels(:a => ["yes","no"], :c => [1,2,4], :d => [1,23,5,7] ,ordered = [:a,:b,:c])
 ```
 """
@@ -19,7 +18,7 @@ Levels(:a => ["yes","no"], :c => [1,2,4], :d => [1,23,5,7] ,ordered = [:a,:b,:c]
   ordered::AbstractVector{Symbol}
 end
 #should work more on constructors
-Levels(pairs::Pair{K}...; ordered::AbstractVector{K}=Symbol[]) where {K <: Symbol}=
+Levels(pairs::Pair{K}...; ordered::AbstractVector{K}=Symbol[]) where {K <: Symbol} =
   Levels(NamedTuple(pairs), ordered)
 
 Levels(pairs::Pair{K}...; ordered::AbstractVector{K}=String[]) where {K <: AbstractString} =
@@ -36,10 +35,10 @@ Levels(pairs::Pair{K}...) where {K <: AbstractString} =
 isrevertible(transform::Levels) = true
 # handle three cases
 #1. when the col is already a categorical array and wanna change levels and order
-categorify(func::AbstractVector, x::CategoricalVector,ordered::Bool) = levels(x) , categorical(x,levels = func,ordered=ordered) 
+categorify(levels::AbstractVector, x::CategoricalVector,ordered::Bool) = levels(x) , categorical(x,levels = levels ,ordered=ordered) 
 #2. when the col is normal array and want to change to categorical array
-categorify(func::AbstractVector, x::AbstractVector,ordered::Bool) = unwrap, categorical(x,levels = func,ordered=ordered) 
-#3. when the col is not need for change 
+categorify(levels::AbstractVector, x::AbstractVector,ordered::Bool) = unwrap, categorical(x,levels = levels,ordered=ordered) 
+#3. when the col is not need for change or convert back to normal array
 categorify(func::Function, x::AbstractVector,ordered::Bool) = ordered ? (levels(x), categorical(x, ordered=true)) : (func , func.(x))
 
 
@@ -47,6 +46,7 @@ function apply(transform::Levels,table)
   cols = Tables.columns(table)
   names = Tables.columnnames(cols)
   levels = transform.levelspec
+  #should I pre-allocate the caches vector
   caches = []
   ncols = map(names) do nm
     x = Tables.getcolumn(cols, nm)
