064746bdfab7b9deea273e64636bb16e1ca75f3a
Elias Carvalho <eliascarvdev@gmail.com>
2022-06-09 08:38:19 -0300

    Add and update old tests

--- test/transforms.jl
+++ test/transforms.jl
@@ -1308,35 +1308,64 @@
     @test tₒ == t
 
     # categorical columns
-    x = categorical(rand(["y", "n"], 50), ordered=false)
-    y = categorical(rand(1:3, 50), ordered=true)
-    t = Table(; x, y)
-    
-    T = Levels(:x => ["n", "y", "m"], :y => 1:4, ordered=r"[xy]")
-    n, c = apply(T, t)
-    @test levels(n.x) == ["n", "y", "m"]
-    @test isordered(n.x) == true
-    @test levels(n.y) == [1, 2, 3, 4]
-    @test isordered(n.y) == true
-    tₒ = revert(T, n, c)
-    @test levels(tₒ.x) == ["n", "y"]
-    @test isordered(tₒ.x) == false
-    @test levels(tₒ.y) == [1, 2, 3]
-    @test isordered(tₒ.y) == true
-    @test tₒ == t
+    a = categorical(["yes", "no", "no", "no", "yes"]) 
+    b = categorical([1, 2, 4, 2, 8], ordered=false) 
+    c = categorical([1, 2, 1, 2, 1]) 
+    d = [1, 23, 5, 7, 7]
+    e = categorical([2, 3, 1, 4, 1])
+    t = Table(; a, b, c, d, e)
 
-    T = Levels("x" => ["n", "y", "m"], "y" => 1:4, ordered=["x"])
+    T = Levels(:a => ["yes", "no"], :c => [1, 2, 4], :d => [1, 23, 5, 7], :e => 1:5)
     n, c = apply(T, t)
-    @test levels(n.x) == ["n", "y", "m"]
-    @test isordered(n.x) == true
-    @test levels(n.y) == [1, 2, 3, 4]
-    @test isordered(n.y) == false
+    @test levels(n.a) == ["yes", "no"]
+    @test levels(n.c) == [1, 2, 4]
+    @test levels(n.d) == [1, 23, 5, 7]
+    @test levels(n.e) == [1, 2, 3, 4, 5]
     tₒ = revert(T, n, c)
-    @test levels(tₒ.x) == ["n", "y"]
-    @test isordered(tₒ.x) == false
-    @test levels(tₒ.y) == [1, 2, 3]
-    @test isordered(tₒ.y) == true
-    @test tₒ == t
+    @test levels(tₒ.a) == ["no", "yes"]
+    @test levels(tₒ.c) == [1, 2]
+    @test levels(tₒ.e) == [1, 2, 3, 4]
+
+    T = Levels("a" => ["yes", "no"], "c" => [1, 2, 4])
+    n, c = apply(T, t)
+    @test levels(n.a) == ["yes", "no"]
+    @test levels(n.c) == [1, 2, 4]
+    tₒ = revert(T, n, c)
+    @test levels(tₒ.a) == ["no", "yes"]
+    @test levels(tₒ.c) == [1, 2]
+
+    T = Levels(:a => ["yes", "no"], :c => [1, 2, 4], :d => [1, 23, 5, 7])
+    n, c = apply(T, t)
+    @test levels(n.a) == ["yes", "no"]
+    @test levels(n.c) == [1, 2, 4]
+    @test levels(n.d) == [1, 23, 5, 7]
+    tₒ = revert(T, n, c)
+    @test levels(tₒ.a) == ["no", "yes"]
+    @test levels(tₒ.c) == [1, 2]
+
+    T = Levels("a" => ["yes", "no"], "c" => [1, 2, 4], "e" => 5:-1:1, ordered=["e"])
+    n, c = apply(T, t)
+    @test levels(n.a) == ["yes", "no"]
+    @test levels(n.c) == [1, 2, 4]
+    @test levels(n.e) == [5, 4, 3, 2, 1]
+    @test isordered(n.a) == false
+    @test isordered(n.c) == false
+    @test isordered(n.e) == true
+    tₒ = revert(T, n, c)
+    @test levels(tₒ.e) == [1, 2, 3, 4]
+    @test isordered(n.e) == false
+
+    T = Levels(:a => ["yes", "no"], :c => [1, 2, 4], :d => [1, 23, 5, 7], ordered=[:a, :d])
+    n, c = apply(T, t)
+    @test levels(n.a) == ["yes", "no"]
+    @test levels(n.c) == [1, 2, 4]
+    @test levels(n.d) == [1, 23, 5, 7]
+    @test isordered(n.a) == true
+    @test isordered(n.c) == false
+    @test isordered(n.d) == true
+    tₒ = revert(T, n, c)
+    @test typeof(tₒ.d) == Vector{Int64}
+    @test isordered(tₒ.a) == false
 
     a = rand([true, false], 50)
     b = rand(["y", "n"], 50)
