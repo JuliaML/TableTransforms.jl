64c7a7a54cacb2dcd9f50e770982fa549df2a226
Elias Carvalho <eliascarvdev@gmail.com>
2022-06-14 09:23:39 -0300

    Add tests

---
+++ test/shows.jl
@@ -0,0 +1,68 @@
+@testset "Shows" begin
+  io = IOBuffer()
+  @testset "Sequential" begin
+    t1 = Select(:x, :z)
+    t2 = ZScore()
+    t3 = Scale(low=0, high=1)
+    pipeline = t1 → t2 → t3
+
+    # compact mode
+    show(io, pipeline)
+    iostr = String(take!(io))
+    @test iostr == "Select{Tuple{Symbol, Symbol}}((:x, :z)) → ZScore() → Scale{Int64}(0, 1)"
+
+    # full mode
+    show(io, MIME("text/plain"), pipeline)
+    iostr = String(take!(io))
+    @test iostr == """
+    Sequential
+    ├─ Select{Tuple{Symbol, Symbol}}((:x, :z))
+    ├─ ZScore()
+    └─ Scale{Int64}(0, 1)"""
+  end
+
+  @testset "Parallel" begin
+    t1 = Scale(low=0.3, high=0.6)
+    t2 = EigenAnalysis(:VDV)
+    t3 = Functional(cos)
+    pipeline = t1 ⊔ t2 ⊔ t3
+
+    # compact mode
+    show(io, pipeline)
+    iostr = String(take!(io))
+    @test iostr == "Scale{Float64}(0.3, 0.6) ⊔ EigenAnalysis(:VDV, nothing, 1.0) ⊔ Functional{typeof(cos)}(cos)"
+
+    # full mode
+    show(io, MIME("text/plain"), pipeline)
+    iostr = String(take!(io))
+    @test iostr == """
+    Parallel
+    ├─ Scale{Float64}(0.3, 0.6)
+    ├─ EigenAnalysis(:VDV, nothing, 1.0)
+    └─ Functional{typeof(cos)}(cos)"""
+
+    # Parallel with Sequential
+    f1 = ZScore()
+    f2 = Scale()
+    f3 = Functional(cos)
+    f4 = Interquartile()
+    pipeline = (f1 → f2) ⊔ (f3 → f4)
+
+    # compact mode
+    show(io, pipeline)
+    iostr = String(take!(io))
+    @test iostr == "ZScore() → Scale{Float64}(0.25, 0.75) ⊔ Functional{typeof(cos)}(cos) → Scale{Float64}(0.25, 0.75)"
+
+    # full mode
+    show(io, MIME("text/plain"), pipeline)
+    iostr = String(take!(io))
+    @test iostr == """
+    Parallel
+    ├─ Sequential
+    │  ├─ ZScore()
+    │  └─ Scale{Float64}(0.25, 0.75)
+    └─ Sequential
+       ├─ Functional{typeof(cos)}(cos)
+       └─ Scale{Float64}(0.25, 0.75)"""
+  end
+end
