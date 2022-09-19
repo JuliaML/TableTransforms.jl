d4fa63c2b05b060e7091d2fe6c377d6385afe4c3
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-09-19 11:01:51 -0300

    Improve comment in assertions.jl

--- src/assertions.jl
+++ src/assertions.jl
@@ -2,7 +2,7 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-# assert that all columns of table are Continuous
+# assert that all columns are continuous
 function assert_continuous(table)
   types = schema(table).scitypes
   @assert all(T <: Continuous for T in types) "columns must hold continuous variables"
