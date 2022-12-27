3eed5b3366eb7d5b08091804195bbf6d00579053
Rajat Chaudhari <rajatrc1705@gmail.com>
2022-12-27 22:48:55 +0100

    adds tests for categorical=false category

--- test/transforms/onehot.jl
+++ test/transforms/onehot.jl
@@ -4,28 +4,31 @@
   c = categorical([3, 2, 2, 1, 1, 3])
   t = Table(; a, b, c)
 
-  T = OneHot(1)
+  T = OneHot(1, false)
   n, c = apply(T, t)
   @test Tables.columnnames(n) == (:a_false, :a_true, :b, :c)
-  @test n.a_false == Bool[1, 0, 0, 1, 0, 0]
-  @test n.a_true  == Bool[0, 1, 1, 0, 1, 1]
+  @test n.a_false == categorical(Bool[1, 0, 0, 1, 0, 0])
+  @test n.a_true  == categorical(Bool[0, 1, 1, 0, 1, 1])
+  @test typeof(n.a_false) != typeof(categorical(Bool[1, 0, 0, 1, 0, 0]))
   tₒ = revert(T, n, c)
   @test t == tₒ
 
-  T = OneHot(:b)
+  T = OneHot(:b, false)
   n, c = apply(T, t)
   @test Tables.columnnames(n) == (:a, :b_f, :b_m, :c)
-  @test n.b_f == Bool[0, 1, 0, 0, 0, 1]
-  @test n.b_m == Bool[1, 0, 1, 1, 1, 0]
+  @test n.b_f == categorical(Bool[0, 1, 0, 0, 0, 1])
+  @test n.b_m == categorical(Bool[1, 0, 1, 1, 1, 0])
+  @test typeof(n.b_f) != typeof(categorical(Bool[0, 1, 0, 0, 0, 1]))
   tₒ = revert(T, n, c)
   @test t == tₒ
 
-  T = OneHot("c")
+  T = OneHot("c", false)
   n, c = apply(T, t)
   @test Tables.columnnames(n) == (:a, :b, :c_1, :c_2, :c_3)
-  @test n.c_1 == Bool[0, 0, 0, 1, 1, 0]
-  @test n.c_2 == Bool[0, 1, 1, 0, 0, 0]
-  @test n.c_3 == Bool[1, 0, 0, 0, 0, 1]
+  @test n.c_1 == categorical(Bool[0, 0, 0, 1, 1, 0])
+  @test n.c_2 == categorical(Bool[0, 1, 1, 0, 0, 0])
+  @test n.c_3 == categorical(Bool[1, 0, 0, 0, 0, 1])
+  @test typeof(n.c_1) != typeof(categorical(Bool[0, 0, 0, 1, 1, 0]))
   tₒ = revert(T, n, c)
   @test t == tₒ
 
@@ -35,11 +38,11 @@
   b_m = rand(10)
   t   = Table(; b, b_f, b_m)
 
-  T = OneHot(:b)
+  T = OneHot(:b, false)
   n, c = apply(T, t)
   @test Tables.columnnames(n) == (:b_f_, :b_m_, :b_f, :b_m)
-  @test n.b_f_ == Bool[0, 1, 0, 0, 0, 1]
-  @test n.b_m_ == Bool[1, 0, 1, 1, 1, 0]
+  @test n.b_f_ == categorical(Bool[0, 1, 0, 0, 0, 1])
+  @test n.b_m_ == categorical(Bool[1, 0, 1, 1, 1, 0])
   tₒ = revert(T, n, c)
   @test t == tₒ
 
@@ -50,16 +53,16 @@
   b_m_ = rand(10)
   t    = Table(; b, b_f, b_m, b_f_, b_m_)
 
-  T = OneHot(:b)
+  T = OneHot(:b, false)
   n, c = apply(T, t)
   @test Tables.columnnames(n) == (:b_f__, :b_m__, :b_f, :b_m, :b_f_, :b_m_)
-  @test n.b_f__ == Bool[0, 1, 0, 0, 0, 1]
-  @test n.b_m__ == Bool[1, 0, 1, 1, 1, 0]
+  @test n.b_f__ == categorical(Bool[0, 1, 0, 0, 0, 1])
+  @test n.b_m__ == categorical(Bool[1, 0, 1, 1, 1, 0])
   tₒ = revert(T, n, c)
   @test t == tₒ
 
   # throws
-  a = categorical(Bool[0, 1, 1, 0, 1, 1])
+  a = categorical(categorical(Bool[0, 1, 1, 0, 1, 1]))
   b = ["m", "f", "m", "m", "m", "f"]
   t = Table(; a, b)
 
