8c916c329ef5f6fbfa85b50604b2241591b27210
Elias Carvalho <eliascarvdev@gmail.com>
2023-08-01 13:11:59 -0300

    Replace 'isequalmissing' by 'isqual'

--- test/transforms/coalesce.jl
+++ test/transforms/coalesce.jl
@@ -25,7 +25,7 @@
   for n in colnames
     col = Tables.getcolumn(cols, n)
     colₒ = Tables.getcolumn(colsₒ, n)
-    @test isequalmissing(col, colₒ)
+    @test isequal(col, colₒ)
   end
 
   # table schema after apply and revert
@@ -49,43 +49,43 @@
   @test Tables.isrowtable(n)
   rtₒ = revert(T, n, c)
   for (row, rowₒ) in zip(rt, rtₒ)
-    @test isequalmissing(row, rowₒ)
+    @test isequal(row, rowₒ)
   end
 
   # colspec
   T = Coalesce(1, 3, 5, value=0)
   n, c = apply(T, t)
   @test n.a == [3, 2, 0, 4, 5, 3]
-  @test isequalmissing(n.b, [missing, 4, 4, 5, 8, 5])
+  @test isequal(n.b, [missing, 4, 4, 5, 8, 5])
   @test n.c == [1, 1, 6, 2, 4, 0]
-  @test isequalmissing(n.d, [4, 3, 7, 5, 4, missing])
+  @test isequal(n.d, [4, 3, 7, 5, 4, missing])
   @test n.e == [0, 5, 2, 6, 5, 2]
-  @test isequalmissing(n.f, [4, missing, 3, 4, 5, 2])
+  @test isequal(n.f, [4, missing, 3, 4, 5, 2])
 
   T = Coalesce([:b, :d, :f], value=0)
   n, c = apply(T, t)
-  @test isequalmissing(n.a, [3, 2, missing, 4, 5, 3])
+  @test isequal(n.a, [3, 2, missing, 4, 5, 3])
   @test n.b == [0, 4, 4, 5, 8, 5]
-  @test isequalmissing(n.c, [1, 1, 6, 2, 4, missing])
+  @test isequal(n.c, [1, 1, 6, 2, 4, missing])
   @test n.d == [4, 3, 7, 5, 4, 0]
-  @test isequalmissing(n.e, [missing, 5, 2, 6, 5, 2])
+  @test isequal(n.e, [missing, 5, 2, 6, 5, 2])
   @test n.f == [4, 0, 3, 4, 5, 2]
 
   T = Coalesce(("a", "c", "e"), value=0)
   n, c = apply(T, t)
   @test n.a == [3, 2, 0, 4, 5, 3]
-  @test isequalmissing(n.b, [missing, 4, 4, 5, 8, 5])
+  @test isequal(n.b, [missing, 4, 4, 5, 8, 5])
   @test n.c == [1, 1, 6, 2, 4, 0]
-  @test isequalmissing(n.d, [4, 3, 7, 5, 4, missing])
+  @test isequal(n.d, [4, 3, 7, 5, 4, missing])
   @test n.e == [0, 5, 2, 6, 5, 2]
-  @test isequalmissing(n.f, [4, missing, 3, 4, 5, 2])
+  @test isequal(n.f, [4, missing, 3, 4, 5, 2])
 
   T = Coalesce(r"[bdf]", value=0)
   n, c = apply(T, t)
-  @test isequalmissing(n.a, [3, 2, missing, 4, 5, 3])
+  @test isequal(n.a, [3, 2, missing, 4, 5, 3])
   @test n.b == [0, 4, 4, 5, 8, 5]
-  @test isequalmissing(n.c, [1, 1, 6, 2, 4, missing])
+  @test isequal(n.c, [1, 1, 6, 2, 4, missing])
   @test n.d == [4, 3, 7, 5, 4, 0]
-  @test isequalmissing(n.e, [missing, 5, 2, 6, 5, 2])
+  @test isequal(n.e, [missing, 5, 2, 6, 5, 2])
   @test n.f == [4, 0, 3, 4, 5, 2]
 end
