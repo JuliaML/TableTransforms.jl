69444cb89b0b4151d58bd2d73390793ae1690219
Elias Carvalho <eliascarvdev@gmail.com>
2023-08-30 16:46:51 -0300

    Add more tests

--- test/shows.jl
+++ test/shows.jl
@@ -104,6 +104,23 @@
     └─ rng = Random._GLOBAL_RNG()"""
   end
 
+  @testset "Map" begin
+    fun = (a, b) -> 2a + b
+    T = Map(:a => sin, [:a, :b] => fun => :c)
+
+    # compact mode
+    iostr = sprint(show, T)
+    @test iostr == "Map(TableTransforms.ColSpec[[:a], [:a, :b]], Function[sin, $(typeof(fun))()], Union{Nothing, Symbol}[nothing, :c])"
+
+    # full mode
+    iostr = sprint(show, MIME("text/plain"), T)
+    @test iostr == """
+    Map transform
+    ├─ colspecs = TableTransforms.ColSpec[[:a], [:a, :b]]
+    ├─ funs = Function[sin, $(typeof(fun))()]
+    └─ targets = Union{Nothing, Symbol}[nothing, :c]"""
+  end
+
   @testset "Filter" begin
     func = row -> row.c ≥ 2 && row.e > 4
     T = Filter(func)
