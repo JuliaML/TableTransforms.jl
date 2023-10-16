d027df8a3596e40af8a61115f89cba2a3f7084a0
Elias Carvalho <eliascarvdev@gmail.com>
2023-10-16 15:24:52 -0300

    Update Coerce tests

--- test/transforms/coerce.jl
+++ test/transforms/coerce.jl
@@ -3,28 +3,28 @@
   b = [1.0, 2.0, 3.0, 4.0, 5.0]
   t = Table(; a, b)
 
-  T = Coerce(1 => DST.Continuous)
+  T = Coerce(1 => DST.Continuous, 2 => DST.Categorical)
   n, c = apply(T, t)
   @test eltype(n.a) <: Float64
-  @test eltype(n.b) <: Float64
+  @test eltype(n.b) <: Int
   n, c = apply(T, t)
   tₒ = revert(T, n, c)
   @test eltype(tₒ.a) == eltype(t.a)
   @test eltype(tₒ.b) == eltype(t.b)
 
-  T = Coerce(:a => DST.Continuous)
+  T = Coerce(:a => DST.Continuous, :b => DST.Categorical)
   n, c = apply(T, t)
   @test eltype(n.a) <: Float64
-  @test eltype(n.b) <: Float64
+  @test eltype(n.b) <: Int
   n, c = apply(T, t)
   tₒ = revert(T, n, c)
   @test eltype(tₒ.a) == eltype(t.a)
   @test eltype(tₒ.b) == eltype(t.b)
 
-  T = Coerce("a" => DST.Continuous)
+  T = Coerce("a" => DST.Continuous, "b" => DST.Categorical)
   n, c = apply(T, t)
   @test eltype(n.a) <: Float64
-  @test eltype(n.b) <: Float64
+  @test eltype(n.b) <: Int
   n, c = apply(T, t)
   tₒ = revert(T, n, c)
   @test eltype(tₒ.a) == eltype(t.a)
@@ -32,7 +32,7 @@
 
   # row table
   rt = Tables.rowtable(t)
-  T = Coerce(:a => DST.Continuous)
+  T = Coerce(:a => DST.Continuous, :b => DST.Categorical)
   n, c = apply(T, rt)
   @test Tables.isrowtable(n)
   rtₒ = revert(T, n, c)
