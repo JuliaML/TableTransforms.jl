7b2c70417e57e07de718121e82954c7cac389d61
Elias Carvalho <eliascarvdev@gmail.com>
2023-11-30 09:56:21 -0300

    Add GeoTables.jl extension

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using TableTransforms
 using CoDa
 using Tables
 using Unitful
+using GeoTables
 using TypedTables
 using CategoricalArrays
 using LinearAlgebra
@@ -43,8 +44,16 @@ Polynomial(args::T...) where {T<:Real} = Polynomial(collect(args))
 include("metatable.jl")
 
 # list of tests
-testfiles =
-  ["distributions.jl", "assertions.jl", "transforms.jl", "metadata.jl", "tableselection.jl", "tablerows.jl", "shows.jl"]
+testfiles = [
+  "distributions.jl",
+  "assertions.jl",
+  "transforms.jl",
+  "extensions.jl",
+  "metadata.jl",
+  "tableselection.jl",
+  "tablerows.jl",
+  "shows.jl"
+]
 
 @testset "TableTransforms.jl" begin
   for testfile in testfiles
