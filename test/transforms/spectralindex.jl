1d9361388d5a4726131a6ffed2684140e82ab758
juliohm <3345261+juliohm@users.noreply.github.com>
2025-07-25 17:11:34 +0000

    :robot: Format .jl files

--- test/transforms/spectralindex.jl
+++ test/transforms/spectralindex.jl
@@ -2,7 +2,7 @@
   @test !isrevertible(SpectralIndex("NDVI"))
 
   # standard names
-  t = Table(R=[1,2,3], N=[4,5,6])
+  t = Table(R=[1, 2, 3], N=[4, 5, 6])
   T = SpectralIndex("NDVI")
   @test T(t).NDVI ≈ [0.6, 0.42857142857142855, 0.3333333333333333]
   T = SpectralIndex("NDVI", R="R")
@@ -13,7 +13,7 @@
   @test T(t).NDVI ≈ [0.6, 0.42857142857142855, 0.3333333333333333]
 
   # some non-standard names
-  t = Table(R=[1,2,3], NIR=[4,5,6])
+  t = Table(R=[1, 2, 3], NIR=[4, 5, 6])
   T = SpectralIndex("NDVI", N="NIR")
   @test T(t).NDVI ≈ [0.6, 0.42857142857142855, 0.3333333333333333]
   T = SpectralIndex("NDVI")
@@ -22,7 +22,7 @@
   @test_throws ArgumentError T(t)
 
   # all non-standard names
-  t = Table(RED=[1,2,3], NIR=[4,5,6])
+  t = Table(RED=[1, 2, 3], NIR=[4, 5, 6])
   T = SpectralIndex("NDVI", R="RED", N="NIR")
   @test T(t).NDVI ≈ [0.6, 0.42857142857142855, 0.3333333333333333]
   T = SpectralIndex("NDVI", R="RED")
@@ -31,7 +31,7 @@
   @test_throws ArgumentError T(t)
 
   # bands as symbols
-  t = Table(RED=[1,2,3], NIR=[4,5,6])
+  t = Table(RED=[1, 2, 3], NIR=[4, 5, 6])
   T = SpectralIndex("NDVI", R=:RED, N=:NIR)
   @test T(t).NDVI ≈ [0.6, 0.42857142857142855, 0.3333333333333333]
 
